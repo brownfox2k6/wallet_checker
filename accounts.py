ff88d42ebdda38f5e46fdf4cf8462603e93c4559
Nguyễn Trọng Đại <24021400@vnu.edu.vn>
2025-05-15 11:04:22 +0700

    added to-do list on accounts.py

--- accounts.py
+++ accounts.py
@@ -4,8 +4,7 @@ from utils import *
 
 def get_ether_balance_single_address(address: str, chainid: int = 1, tag: str = "latest") -> float:
   """
-  [Get Ether Balance for a Single Address](https://docs.etherscan.io/etherscan-v2/api-endpoints/accounts#get-ether-balance-for-a-single-address)
-
+  [Get Ether Balance for a Single Address](https://docs.etherscan.io/etherscan-v2/api-endpoints/accounts#get-ether-balance-for-a-single-address) \n
   Returns: Ether balance of `address`
   """
   url = get_url(chainid=chainid, module="account", action="balance", address=address, tag=tag)
@@ -17,8 +16,7 @@ def get_ether_balance_single_address(address: str, chainid: int = 1, tag: str = 
 
 def get_ether_balance_multiple_addresses(addresses: list[str], chainid: int = 1, tag: str = "latest") -> list[float]:
   """
-  [Get Ether Balance for Multiple Addresses in a Single Call](https://docs.etherscan.io/etherscan-v2/api-endpoints/accounts#get-ether-balance-for-multiple-addresses-in-a-single-call)
-
+  [Get Ether Balance for Multiple Addresses in a Single Call](https://docs.etherscan.io/etherscan-v2/api-endpoints/accounts#get-ether-balance-for-multiple-addresses-in-a-single-call) \n
   Returns: A list of Ether balances corresponding to each address in addresses.
   """
   address = ','.join(addresses)
@@ -30,3 +28,43 @@ def get_ether_balance_multiple_addresses(addresses: list[str], chainid: int = 1,
     ether = wei_to_ether(item["balance"])
     ether_values.append(ether)
   return ether_values
+
+
+def get_normal_transactions() -> list[dict]:
+  pass
+
+
+def get_internal_transactions_by_address() -> list[dict]:
+  pass
+
+
+def get_internal_transactions_by_transaction_hash() -> list[dict]:
+  pass
+
+
+def get_internal_transactions_by_block_range() -> list[dict]:
+  pass
+
+
+def get_erc20_token_transfer_events() -> list[dict]:
+  pass
+
+
+def get_erc721_token_transfer_events() -> list[dict]:
+  pass
+
+
+def get_erc1155_token_transfer_events() -> list[dict]:
+  pass
+
+
+def get_address_funded_by() -> list[dict]:
+  pass
+
+
+def get_blocks_validated() -> list[dict]:
+  pass
+
+
+def get_beacon_chain_withdrawals() -> list[dict]:
+  pass
