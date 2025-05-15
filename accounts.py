a393d3c68f9a0dc99290c7a1c68c87230d1192ba
Nguyễn Trọng Đại <brfox2k6@gmail.com>
2025-05-15 10:50:21 +0700

    Added 2 functions
    - get_ether_balance_single_address
    - get_ether_balance_multiple_addresses

--- accounts.py
+++ accounts.py
@@ -1,7 +1,32 @@
-import requests, utils
+import requests
+from utils import *
 
-def get_balance(chainid, module, action, address, tag, apikey):
-  url = utils.get_url(chainid=chainid, module=module, action=action, address=address, tag=tag, apikey=apikey)
-  gwei = int(requests.get(url).json()["result"])
-  eth = gwei // 10**18
-  return eth
+
+def get_ether_balance_single_address(address: str, chainid: int = 1, tag: str = "latest") -> float:
+  """
+  [Get Ether Balance for a Single Address](https://docs.etherscan.io/etherscan-v2/api-endpoints/accounts#get-ether-balance-for-a-single-address)
+
+  Returns: Ether balance of `address`
+  """
+  url = get_url(chainid=chainid, module="account", action="balance", address=address, tag=tag)
+  response = requests.get(url)
+  data = response.json()
+  ether = wei_to_ether(data["result"])
+  return ether
+
+
+def get_ether_balance_multiple_addresses(addresses: list[str], chainid: int = 1, tag: str = "latest") -> list[float]:
+  """
+  [Get Ether Balance for Multiple Addresses in a Single Call](https://docs.etherscan.io/etherscan-v2/api-endpoints/accounts#get-ether-balance-for-multiple-addresses-in-a-single-call)
+
+  Returns: A list of Ether balances corresponding to each address in addresses.
+  """
+  address = ','.join(addresses)
+  url = get_url(chainid=chainid, module="account", action="balancemulti", address=address, tag=tag)
+  response = requests.get(url)
+  data = response.json()
+  ether_values = []
+  for item in data["result"]:
+    ether = wei_to_ether(item["balance"])
+    ether_values.append(ether)
+  return ether_values
--- utils.py
+++ utils.py
@@ -1,5 +1,16 @@
 API_KEY = "your_api_key"
 
+
 def get_url(**kwargs):
-  url = "https://api.etherscan.io/v2/api?" + ''.join(f'&{key}={value}' for key, value in kwargs.items())
+  url = "https://api.etherscan.io/v2/api?"
+  url += ''.join(f"&{key}={value}" for key, value in kwargs.items())
+  url += f"&apikey={API_KEY}"
   return url
+
+
+def wei_to_ether(wei: int | str) -> float:
+  """
+  Convert Wei to Ether: 
+  based on https://etherscan.io/unitconverter, 1 Ether = 1e18 Wei
+  """
+  return int(wei) / 1e18
